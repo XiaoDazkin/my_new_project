ac56b804c7c365a67e859b3f6e9759a912bf12f7
XiaoDazkin <pwynizpanda@gmail.com>
2025-06-29 13:35:37 +0700

    Mod Floor Division

---
+++ modfloordvd.py
@@ -0,0 +1,7 @@
+a = int(input("Enter number for a: "))
+b = int(input("Enter number for b: "))
+
+print("--------Results--------")
+print(f"{a} / {b} = {a / b} - Normal Division")
+print(f"{a} // {b} = {a // b} - Floor Division")
+print(f"{a} % {b} = {a % b} - Modulo Division")
